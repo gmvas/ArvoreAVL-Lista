37a7e5a87280f69215d3430a935c0c2e3fba38b2
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-06-09 21:19:06 -0300

    Adição de métodos da Arvore/Lista

--- ArvoreAVL.java
+++ ArvoreAVL.java
@@ -1,45 +1,73 @@
+import javax.sound.midi.MidiChannel;
+
 public class ArvoreAVL {
-    private NoCelula raiz;
+    private No raizA;
+    private ListaDupla raizL;
 
-    private NoCelula inserir(NoCelula raiz, NoCelula novo) {
-        if(raiz == null) { //Situação de nova insercao na arvore
-            raiz = novo; 
+    private No inserir(No raizA, No novo) {
+        if(raizA == null) { //Situação de nova insercao na arvore
+            raizA = novo; 
         }
-        else if(novo.nome.compareToIgnoreCase(raiz.nome) < 0) //Buscando caminho de itens menores
-            raiz.Aesq = inserir(novo, raiz.Aesq);
-        else if(novo.nome.compareToIgnoreCase(raiz.nome) > 0) //Buscando caminho de itens maiores
-            raiz.Adir = inserir(novo, raiz.Adir);
-        else incrementaContador(raiz, novo);
+        else if(novo.nome.compareToIgnoreCase(raizA.nome) < 0) //Buscando caminho de itens menores
+            raizA.esq = inserir(novo, raizA.esq);
+        else if(novo.nome.compareToIgnoreCase(raizA.nome) > 0) //Buscando caminho de itens maiores
+            raizA.dir = inserir(novo, raizA.dir);
+        else incrementaContador(raizA, novo);
 
-        return raiz;
+        return raizA;
     }
 
     public void inserir(String nome) {
-        NoCelula novo = new NoCelula(nome);
-        raiz = inserir(raiz, novo);
+        No novo = new No(nome);
+        raizA = inserir(raizA, novo);
     }
 
-    private NoCelula incrementaContador(NoCelula raiz, NoCelula buscando) {
-        if(raiz!=null) {
-            if(raiz.nome == buscando.nome) { //Busca pelo conteudo desejado
-                raiz.contador++;
-                return raiz;
+    private No incrementaContador(No raizA, No buscando) {
+        if(raizA!=null) {
+            if(raizA.nome == buscando.nome) { //Busca pelo conteudo desejado
+                raizA.contador++;
+                return raizA;
             }
-            else if(buscando.nome.compareToIgnoreCase(raiz.nome) < 0) //Caminha a esquerda (nomes menores)
-                raiz.Aesq = incrementaContador(buscando, raiz.Aesq);
-            else if(buscando.nome.compareToIgnoreCase(raiz.nome) > 0) //Caminha a direita (nomes maiores)
-                raiz.Adir = incrementaContador(buscando, raiz.Adir);
+            else if(buscando.nome.compareToIgnoreCase(raizA.nome) < 0) //Caminha a esquerda (nomes menores)
+                raizA.esq = incrementaContador(buscando, raizA.esq);
+            else if(buscando.nome.compareToIgnoreCase(raizA.nome) > 0) //Caminha a direita (nomes maiores)
+                raizA.dir = incrementaContador(buscando, raizA.dir);
         }
-        return raiz; //Return para efetivar a alteração dentro da arvore
+        return raizA; //Return para efetivar a alteração dentro da arvore
     }
 
-    private void listar(NoCelula raiz) { //Listagem por caminhamento central
-        listar(raiz.Aesq);
-        System.out.println("- " + raiz.nome);
-        listar(raiz.Adir);
+    private void listarArvore(No raizA) { //Listagem por caminhamento central
+        listarArvore(raizA.esq);
+        System.out.println("- " + raizA.nome);
+        listarArvore(raizA.dir);
     }
 
-    public void listar() {
-        listar(raiz);
+    private ListaDupla montarLista(No raizA, ListaDupla raizL) {
+        if(raizA != null){ //Checando por conteudo dentro do No
+            raizL.conteudo = raizA; //Pegando conteudo do No sem fazer ordenação
+            montarLista(raizA.esq, raizL.prox);
+            montarLista(raizA.dir, raizL.prox);
+        }
+        return raizL;
+    }
+
+    public void montarLista() { //Tentando achar um jeito que de certo
+        ListaDupla aux = new ListaDupla();
+        raizL = montarLista(raizA, aux);
+        while (aux.conteudo != null) {
+            
+        }
+    }
+
+    public void listarArvore() {
+        listarArvore(raizA);
+    }
+
+    private void listarLista(No raizA) {
+        System.out.println();
+    }
+
+    public void listarLista() {
+        listarArvore(raizA);
     }
 }
