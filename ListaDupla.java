373d12ca39c0df338a67478464d2bda2c1f263d4
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-06-12 12:56:11 -0300

    Revert "Mudança de funções"
    This reverts commit 522f0589a1200b42af46b2a7085fa3db20eb3524.

--- ListaDupla.java
+++ ListaDupla.java
@@ -1,34 +1,11 @@
 public class ListaDupla {
-    private No cabeca; //Maior elemento da Lista - primeiro elemento
-    private No cauda; //Menor elemento da Lista - ultimo elemento
+    ListaDupla prox;
+    ListaDupla ant;
+    No conteudo;
 
-    ListaDupla() {
-        cabeca = new No();
-        cauda = new No();
-    }
-
-    /*
-     *  TODO: verificar se utilizar esse metodo dentro da ListaDupla vai funcionar
-     *  possivel problema com a chamada da 'ArvoreAVL arvore' onde pode impedir o
-     *  caminhamento do No da Árvore
-     * 
-     *  Caso nao funcione, transferir funcao para a ArvoreAVL e fazer as 
-     *  respectivas mudancas necessarias
-     */
-    public void obterConteudo(ArvoreAVL arvore) { //Metodo para copiar a Arvore para a Lista
-        if(cabeca.nome == null) { //Caso de primeira insercao
-            cabeca = new No(arvore.getRaiz());
-            cabeca.dir = cauda;
-            cauda.esq = cabeca;
-        }
-    }
-
-    public void listar(No cabeca) {
-        if(cabeca!=null){ //Verifica pela existencia de conteudo
-            System.out.print("Nome: " + cabeca.nome);
-            System.out.println(" - Frequencia: " + cabeca.contador);
-            listar(cabeca.dir); //Caminha para o proximo No
-        }
-        
+    ListaDupla(){
+        prox = null;
+        ant = null;
+        conteudo = null;
     }
 }
--- No.java
+++ No.java
@@ -1,8 +1,8 @@
 public class No {
     String nome;
     int contador = 0;
-    No esq; //No a esquerda
-    No dir; //No a direita
+    No esq; //Arvore esquerda
+    No dir; //Arvore direita
 
     No() { //Contrutor base
         this.nome = null;
@@ -10,17 +10,10 @@ public class No {
         dir = null;
     }
 
-    No(String nome) { //Construtor com conteudo - para arvore binaria
+    No(String nome) { //Construtor com conteudo
         this.nome = nome.toUpperCase(); //Strings todas em letra maiscula afim de não haver conflitos de nome por conta de letras maiusculas
         contador++; //Novo nome indica uma aparicao de pelo menos 1 vez
         esq = null;
         dir = null;
-    }
-
-    No(No existente) { //Construtor para criacao de Nos da ListaDupla
-        this.nome = existente.nome;
-        this.contador = existente.contador;
-        this.esq = null;
-        this.dir = null;
     }
 }
