802234116093fd4bebca3b34871c23adf2c6c43a
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-06-12 22:36:17 -0300

    Desenvolvimento LIsta
    Desenvolvimento dos metodos da ListaDupla de leitura da Arvore

--- ListaDupla.java
+++ ListaDupla.java
@@ -1,51 +1,51 @@
 public class ListaDupla {
-    No cabeca; //Maior elemento da Lista - primeiro elemento
-    No cauda; //Menor elemento da Lista - ultimo elemento
+    No primeiro; //Maior elemento da Lista - primeiro elemento
+    No ultimo; //Menor elemento da Lista - ultimo elemento
 
     ListaDupla() {
-        cabeca = new No();
-        cauda = new No();
+        primeiro = new No();
+        ultimo = primeiro;
     }
 
-    /*
-     *  TODO: verificar se utilizar esse metodo dentro da ListaDupla vai funcionar com o professor
-     */
-    public void obterConteudo(No raizArvore, No cabeca, No cauda) { //Metodo para copiar a Arvore para a Lista - deve ser usado no main
-        if(cabeca.nome == null) { //Primeira inserção na Lista
-            cabeca = new No(raizArvore);
-            cabeca.dir = cauda;
-            cauda.esq = cabeca;
-        }
-        else if(cauda.nome == null) { //Primeira inserção na Cauda
-            cauda = new No(raizArvore);
-        }
-        else transfConteudoNo(raizArvore, cauda); //Inserção no meio da Lista
+    public void obterConteudo(No raizArvore, No primeiro, No ultimo) { //Metodo para copiar a Arvore para a Lista - deve ser usado no main
+        if(primeiro.dir == null) { //Primeira inserção na Lista
+            No aux = new No(raizArvore);
+            aux.dir = primeiro.esq;
+            aux.esq = primeiro;
+            primeiro.dir = aux;
+            ultimo = aux;
+        }   
+        else primeiro = transfConteudoNo(raizArvore, ultimo); //Inserção no meio da Lista
     }
 
-    public No transfConteudoNo(No raizArvore, No raizLista) { //Metodo para transferir conteudo entre Nos
-        if(raizArvore.contador > raizLista.contador) { //Caminhamento para achar o devido local
+    private No transfConteudoNo(No raizArvore, No raizLista) { //Metodo para transferir conteudo entre Nos
+        if(raizArvore.contador > raizLista.esq.contador && raizArvore.contador > raizLista.contador) { //Caminhamento para achar o devido local
             raizLista = transfConteudoNo(raizArvore, raizLista.dir);
         }
         else {
             //Transferencia de conteudos de No e de apontadores
-            No aux = new No(raizLista);
+            No aux = new No();
             aux.esq = raizLista.esq;
             aux.dir = raizLista;
-            raizLista.esq.dir = aux;    
+               
             raizLista.esq = aux;
+            aux.esq.dir = aux;
 
             //Transferindo conteudos da raiz da Arvore para a Lista
-            raizLista = raizArvore;
+            aux.nome = raizArvore.nome;
+            aux.contador = raizArvore.contador;
+
+            aux = null; // Esvaziando No auxiliars (seguindo como indicado no slide)
         }
 
         return raizLista;
     }
 
-    public void listar(No cabeca) {
-        if(cabeca!=null){ //Verifica pela existencia de conteudo
-            System.out.print("Nome: " + cabeca.nome);
-            System.out.println(" - Frequencia: " + cabeca.contador);
-            listar(cabeca.dir); //Caminha para o proximo No
+    public void listar(No primeiro) {
+        if(primeiro!=null){ //Verifica pela existencia de conteudo
+            System.out.print("Nome: " + primeiro.nome);
+            System.out.println(" - Frequencia: " + primeiro.contador);
+            listar(primeiro.dir); //Caminha para o proximo No
         }
         
     }
